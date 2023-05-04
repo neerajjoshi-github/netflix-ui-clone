fe3b9e8f3c39a645b91310808f9e2050c546c3b9
neerajjoshi-github <joshineeraj7037@gmail.com>
2023-05-04 21:25:18 +0530

    connected react hook form and zod to all the forms

--- app/dashboard/browse_by_languages/page.tsx
+++ app/dashboard/browse_by_languages/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ImageBillboard from "<@>/components/ImageBillboard";
 
 const page = () => {
-  return <div>page</div>;
+  return <div className="pb-32"></div>;
 };
 
 export default page;
